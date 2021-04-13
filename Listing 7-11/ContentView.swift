efe6b6e7aedf70af26f04db065407dcfa5961519
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2021-04-13 21:13:29 +0300

    End project

--- Listing 7-11/ContentView.swift
+++ Listing 7-11/ContentView.swift
@@ -1,21 +1,46 @@
-//
-//  ContentView.swift
-//  Listing 7-11
-//
-//  Created by John Gauchat on 2020-01-31.
-//  Copyright © 2020 John Gauchat. All rights reserved.
-//
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+   @State private var title: String = "Default Title"
+   @State private var titleInput: String = ""
+
+   var body: some View {
+      VStack {
+         HeaderView(title: $title)
+         TextField("Insert Title", text: $titleInput)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+         Button(action: {
+            self.title = self.titleInput
+            self.titleInput = ""
+         }, label: { Text("Change Title") })
+         Spacer()
+      }.padding()
+   }
+}
+struct HeaderView: View {
+   @Binding var title: String
+   var counter: Int = 0
+
+   init(title: Binding<String>) {
+      self._title = title
+        
+      let sentence = self._title.wrappedValue
+      counter = sentence.count
+   }
+   var body: some View {
+      Text("\(title) (\(counter))")
+         .padding(10)
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct HeaderView_Previews: PreviewProvider {
+   static var previews: some View {
+      HeaderView(title: .constant("My Preview Title"))
+   }
 }
